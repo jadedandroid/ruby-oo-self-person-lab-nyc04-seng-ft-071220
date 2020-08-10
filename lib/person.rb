42e406794954469ed211cf5f24166f86c9990ce4
jade decordova <lacroixjaded@gmail.com>
2020-08-10 12:45:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,11 +9,11 @@ class Person
     @bank_account = 25
     @happiness = 8.0 
     @hygiene = 8.0
-    
+    @hygiene != 0 || @hygiene != 10
+    @happiness != 0 || @happiness != 10
   end
   
   def clean?
-    
     if @hygiene > 7.0
       true 
     elsif @hygiene < 0 || @hygiene > 10 
@@ -29,21 +29,24 @@ class Person
   end
     
   def get_paid(salary)
-    puts "#{@account + salary} all about the benjamins"
+    self.bank_account += salary
+    return "all about the benjamins"
   end
   
   def take_bath
-    @hygiene +=4
-    puts "♪ Rub-a-dub just relaxing in the tub ♫"
+    @hygiene != 0 || @hygiene != 10
+    @hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
   end
   
   def work_out
     @happiness +=2
     @hygiene  -= 3
-     puts "♪ another one bites the dust ♫"  
+     return "♪ another one bites the dust ♫"  
   end
   
-  # def call_friend
+  # def call_friend(person)
+    
          
     
   # end
